39f56754a0b3e030f6142b579ff5bbab35bb250b
harshabacham <harshabacham3@gmail.com>
2025-06-27 11:22:15 +0530

    Add start.sh script

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 # Add src/ to Python path
 export PYTHONPATH="${PYTHONPATH}:./src"
 # Start FastAPI from the correct location
-uvicorn src.book_ai_backend.main:app --host 0.0.0.0 --port ${PORT:-10000}cd src && uvicorn book_ai_backend.main:app --host 0.0.0.0 --port $PORT
+#!/bin/bash
+uvicorn main:app --host 0.0.0.0 --port 10000
